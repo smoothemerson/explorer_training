06be0b665560c20917421302ca866406a03b1f0d
emerson <emerson.faria@serpro.gov.br>
2024-12-23 15:04:50 -0300

    feat: add e-tasks project

---
+++ extra_contents/e-tasks/src/components/SideMenu/index.jsx
@@ -0,0 +1,38 @@
+import { CheckCircle, House, Folder, PlusSquare, Note, SignOut, X } from "@phosphor-icons/react";
+
+import { Container, Footer, Nav, Title, Header, Button } from "./styles";
+
+export function SideMenu({ menuIsOpen, onCloseMenu }) {
+  return (
+    <Container data-menu-is-open={menuIsOpen}>
+      <Header>
+        <Title><CheckCircle /> e-Tasks</Title>
+
+        {menuIsOpen &&
+          <Button onClick={onCloseMenu}>
+            <X />
+          </Button>
+        }
+      </Header>
+
+      <Nav>
+        <a href="#" data-menu-active="true"><House /> Home</a>
+        <a href="#"><PlusSquare /> Nova tarefa</a>
+        <a href="#"><Folder /> Projetos</a>
+        <a href="#"><Note /> Relatórios</a>
+      </Nav>
+
+      <Footer>
+        <img src="https://github.com/smoothemerson.png" alt="Foto do usuário" />
+        <div>
+          <strong>Emerson rocha</strong>
+          <small>emerson@email.com</small>
+        </div>
+
+        <Button type="button">
+          <SignOut />
+        </Button>
+      </Footer>
+    </Container>
+  );
+}
